de0c9fba4d5788405a04bfe5ccda35f3f43d9083
Ann Maria Cherian <annmarie@tendercare.me>
2019-07-08 15:02:19 +0400

    start application done login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { CustomMaterialModule } from './core/material.module';
+import { AppRoutingModule } from './core/app.routing.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './signup/signup.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomMaterialModule,
+    FormsModule,
+    RouterModule.forRoot([]),
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/app.routing.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {UserComponent} from '../user/user.component';
+import {LoginComponent} from '../login/login.component';
+import { SignupComponent } from '../signup/signup.component';
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent },
+
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
